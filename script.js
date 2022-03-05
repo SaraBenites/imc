1258c86dda1bb9edba5f085b44929f949c22eb65
Sara Benites <sarabenites60@gmail.com>
2022-03-05 17:48:59 -0500

    IMC

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = iniciar;
+
+<script type="text/javascript">
+    <calc = document.getElementById > </script> </calc> ("calc");
+	kg = document.getElementById("KG");
+	m = document.getElementById("M");
+	imc = document.getElementById("IMC");
+	lectura = document.getElementById("lectura");
+
+
+</script>
+    
